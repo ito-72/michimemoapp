d79cc1f24963470d362b92337f6265fc64e8dee3
ito-72 <153911389+ito-72@users.noreply.github.com>
2025-03-24 17:02:42 +0900

    Add files via upload

--- api/calculateWeeklyDistance.js
+++ api/calculateWeeklyDistance.js
@@ -1,5 +1,5 @@
 // GASのエンドポイントURL（★あなたのURLに差し替えてください）
-const GAS_URL ="https://script.google.com/macros/s/AKfycby8UGsJ_65JvZwD3S5xudzjFbFnwKYxYDe6OutmHvHa4iprCcdPwkxaxn0GGuS9NLBW/exec"
+const GAS_URL ="https://script.google.com/macros/s/AKfycby1jAqKyJYdJkGmI-FhfGK5XGleD3MroYzjLt2RP3_g76zPfxNXQf5stHU2nsGyMQYi/exec"
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
